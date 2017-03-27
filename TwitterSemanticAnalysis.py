5b3a775699148e10162a5d3638f87e60f3ae504e
florian dahlitz <f2dahlitz@freenet.de>
2017-03-27 20:48:33 +0200

    works now also w/ empty entries

--- TwitterSemanticAnalysis.py
+++ TwitterSemanticAnalysis.py
@@ -26,12 +26,20 @@ class App(Tk):
         self.number_label.pack()
         self.number.pack()
 
+    def check_empty(self, term, number):
+        if len(term) == 0:
+            term = "Twitter"
+        if len(number) == 0:
+            number = 100
+        return {1: term, 2: number}
+
     def click_submit(self):
+        values = self.check_empty(term=self.term.get(), number=self.number.get())
         r_source = ro.r["source"]
         curr_dir = os.path.dirname(__file__)
         ranalytics = r_source(curr_dir + "/ranalytics/ranalytics.R")
         do_analysis = ranalytics[0]
-        do_analysis(self.term.get(), number=self.number.get())
+        do_analysis(values[1], number=values[2])
 
 root = App()
 root.title("Twitter Semantic Analysis")
